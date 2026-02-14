50185f8f92858138a8b790d974aa0e6fc8a2ee7d
jjjjustine <catingan.janejustine@gmail.com>
2026-02-14 18:50:08 +0800

    added signup

---
+++ signup.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Create Account</title>
+</head>
+<body>
+
+<h2>Sign Up</h2>
+
+<form action="register.php" method="POST">
+  Username: <input type="text" name="username" required><br><br>
+  Password: <input type="password" name="password" required><br><br>
+  <button type="submit">Create Account</button>
+</form>
+
+<p><a href="login.php">Already have an account? Login</a></p>
+
+</body>
+</html>
